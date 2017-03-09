462182e139d12c39bbabcd54d862514c70c8320d
Jan Vonde <jan.vonde@intranda.com>
2017-03-09 07:00:07 +0100

    added better output and perfdata

--- README.md
+++ README.md
@@ -24,6 +24,11 @@ object CheckCommand "needsrestarting" {
 ```
 
 
+### Changelog
+* 2017-03-09: added better output and perfdata
+* 2017-03-04: initial version
+
+
 ### License
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
--- check_needsrestarting.sh
+++ check_needsrestarting.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #####
 #
 # Monitoring plugin to check if programms need to be restarted on CentOS or other RHEL based distributions.
@@ -12,30 +11,34 @@
 #
 #
 # Changelog:
+#   2017-03-09: added better output and perfdata
 #   2017-03-04: initial version
 #
 # For more information visit https://github.com/janvonde/check_needsrestarting
 #####
 
 
-## error handling
+
 type -P needs-restarting &>/dev/null || { echo "ERROR: needs-restarting is required but seems not to be installed. (yum-goodies)  Aborting." >&2; exit 1; }
 
 
+
 OUTPUT=$(needs-restarting 2>&1)
+LINECOUNT=$(echo "${OUTPUT}" | wc -l)
 RETURNCODE=$?
 
 
+
 if [ "${RETURNCODE}" != "0" ]; then
-	echo "CRITICAL: needs-restarting exited with ${RETURNCODE} ${OUTPUT}";
+	echo "CRITICAL: needs-restarting exited with ${RETURNCODE} \n ${OUTPUT} | processes=${LINECOUNT}";
 	exit 2
 fi
 
 if [ "${OUTPUT}" != "" ]; then
-	echo "WARNING: ${OUTPUT}";
+	echo "WARNING: ${LINECOUNT} processes need to be restarted \n ${OUTPUT} | processes=${LINECOUNT}";
 	exit 1
 fi
 
-echo "OK";
+echo "OK: No processes need to be restarted | processes=${LINECOUNT}";
 exit 0
 
